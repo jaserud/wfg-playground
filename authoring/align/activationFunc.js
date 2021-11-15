1f65a14119614cb20b70d41248f49fb6f33be5d1
jaserud <udjaser@gmail.com>
2021-11-15 17:09:07 -0500

    align, add missiing params

--- authoring/align/activationFunc.js
+++ authoring/align/activationFunc.js
@@ -1,14 +1,13 @@
 module.exports = function (data) {
   const currAnalysis = data.data.analyses.content[0];
   if (!currAnalysis) {
-    reject(`Analysis is empty!`);
+    return reject(`Analysis is empty!`);
   }
 
-  const isAnalysisBeenAligned =
-    currAnalysis.completedAlignRuns.length > 0 &&
-    currAnalysis.completedAlignRuns[0].publishedAlignAnalyses.length > 0;
-  if (isAnalysisBeenAligned) {
-    reject(`Analysis with ID ${currAnalysis.analysisId} has already been aligned!`);
+  for (const run of currAnalysis.completedRuns) {
+    if (run.publishedAnalyses.length > 0) {
+      return reject(`Analysis with ID ${currAnalysis.analysisId} has already been aligned!`);
+    }
   }
 
   return {
@@ -16,6 +15,8 @@ module.exports = function (data) {
     study_id: currAnalysis.studyId,
     score_url: 'https://score.rdpc-dev.cancercollaboratory.org',
     song_url: 'https://song.rdpc-dev.cancercollaboratory.org',
+    cpus: 2,
+    mem: 4,
     ref_genome_fa: '<SCHEDULED_DIR>/reference/GRCh38_hla_decoy_ebv/GRCh38_hla_decoy_ebv.fa',
     download: {
       song_cpus: 2,
@@ -25,7 +26,53 @@ module.exports = function (data) {
       score_url: 'https://submission-score.rdpc-dev.cancercollaboratory.org',
       song_url: 'https://submission-song.rdpc-dev.cancercollaboratory.org',
     },
-    cpu: 6,
-    mem: 18,
+    seqDataToLaneBam: {
+      cpus: 4,
+      mem: 12,
+    },
+    bwaMemAligner: {
+      cpus: 6,
+      mem: 18,
+    },
+    bamMergeSortMarkdup: {
+      cpus: 4,
+      mem: 18,
+    },
+    payloadGenDnaAlignment: {
+      cpus: 2,
+      mem: 4,
+    },
+    readGroupUBamQC: {
+      cpus: 3,
+      mem: 6,
+    },
+    alignedSeqQC: {
+      cpus: 4,
+      mem: 10,
+    },
+    gatkCollectOxogMetrics: {
+      cpus: 3,
+      mem: 6,
+    },
+    payloadGenDnaSeqQc: {
+      cpus: 2,
+      mem: 2,
+    },
+    uploadAlignment: {
+      song_cpus: 2,
+      song_mem: 2,
+      score_cpus: 4,
+      score_mem: 10,
+    },
+    uploadQc: {
+      song_cpus: 2,
+      song_mem: 2,
+      score_cpus: 2,
+      score_mem: 4,
+    },
+    tempdir: '/icgc-argo-scratch',
+    cleanup: true,
+    max_retries: 5,
+    first_retry_wait_time: 60,
   };
 };
